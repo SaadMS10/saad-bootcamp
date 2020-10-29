bb71f2bd6140f875a068fd237a918162c7621a53
SaadMS10 <itsmsaadhere@gmail.com>
2020-10-29 20:38:31 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Saad Hello
+          waleed hello
         </p>
 
       </header>
