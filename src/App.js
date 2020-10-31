a9442a17de1991fb829ce1b32f5c16fbb0355f81
SaadMS10 <itsmsaadhere@gmail.com>
2020-10-31 18:33:32 +0500

    Footballer File Added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './App.css';
-import Dinner from './dinner.js';
+import Footballer from './footballer.js';
 function App() {
   return (
     <div className='App'>
-      {/* <Dinner></Dinner> */}
-      <Dinner dishName='Nihaari' sweetDish='Kheer' />
+      <h1>Hello Welcome To React APP</h1>
+      <h1>The List Of The Some Greatest Footballer</h1>
       <hr />
-      <Dinner dishName='Biryani' sweetDish='Jaleebi' />
+      {/* <Footbller></Footballer> */}
+      <Footballer no="1" Name="Messi"  Club="Barcelona"/>
       <hr />
-      <Dinner dishName='Karahi' sweetDish='Gajar ka Halwa' />
+      <Footballer no="2" Name="Cristiano"  Club="Madrid"/>
+      <hr />
+      <Footballer no="3" Name="Maradona"  Club="Barcelona,Napoli"/>
+      <hr />
     </div>
   );
 }
---
+++ src/footballer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+function Footballer(props) {
+  return (
+    <div>
+      
+      <h2>{props.no} Name:{props.Name} Club:{props.Club}</h2>
+      <hr/>
+    
+    </div>
+  );
+}
+export default Footballer;
